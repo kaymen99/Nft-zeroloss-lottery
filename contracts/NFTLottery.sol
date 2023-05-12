a24cd11981753c2c2b3727deea43230431d821ba
Aymen <AymenMir1001@gmail.com>
2023-05-12 23:38:12 +0100

    Update NFTLottery.sol

--- contracts/NFTLottery.sol
+++ contracts/NFTLottery.sol
@@ -2,55 +2,53 @@
 
 pragma solidity 0.8.10;
 
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+import "./utils/ChainlinkUtils.sol";
 import "./interfaces/INFTCollection.sol";
 import "./interfaces/IPool.sol";
 
-contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
+contract NFTLottery is ChainlinkUtils, Ownable {
     //--------------------------------------------------------------------
     // VARIABLES
 
-    // chainlink keepers parameters
-    VRFCoordinatorV2Interface private immutable vrfCoordinator;
-    uint64 private immutable subscriptionId;
-    bytes32 private immutable gasLane;
-    uint32 private immutable callbackGasLimit;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
-
-    // USE uint256 instead of bool to save gas
-    // paused = 1 & active = 2
-    uint256 public paused = 1;
+    address[] private participants;
+    mapping(address => Participant) isParticipant;
 
     uint256 private _totalDAIDeposited;
-    uint256 private _lotteryStartTimestamp;
-    LOTTERY_STATE private _lotteryState = LOTTERY_STATE.CLOSED;
 
-    uint256 public lotteryPeriod = 1 days;
-    uint256 public lotteryDelay = 1 hours;
-    uint256 public lotteryNftRewardCount = 1;
-    
-    uint256 public ticketBasePrice = 100 * 1e18; // the price of the ticket in DAI (100 DAI)
-    uint256 public winnersPerLottery = 1;
-    
+    struct LotteryParams {
+        uint32 nftRewardCount;
+        uint32 winnersPerRound;
+        uint32 delay;
+        uint64 startTimestamp;
+        uint64 duration;
+        uint256 ticketPrice;
+    }
+
+    struct State {
+        uint32 nftRewardCount;
+        uint32 winnersPerRound;
+        uint32 delay;
+        uint64 startTimestamp;
+        uint64 duration;
+        bool paused;
+        LOTTERY_STATUS status;
+        uint256 ticketPrice;
+    }
+    State public lotteryState;
+
     IERC20 immutable dai;
     IERC20 immutable aDai;
     IPool immutable pool;
     INFTCollection immutable nft;
 
-    address[] private participants;
-    mapping(address => Participant) isParticipant;
-
     struct Participant {
-        uint256 id;
+        uint224 id;
         bool hasParticipated;
     }
 
-    enum LOTTERY_STATE {
+    enum LOTTERY_STATUS {
         OPEN,
         CLOSED,
         CALCULATING_WINNER
@@ -71,12 +69,12 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     event Lottery__RequestWinners(uint256 requestId);
     event Lottery__TicketPriceChanged(uint256 ticketPrice, uint256 timestamp);
     event Lottery__WinnersPerLotteryChanged(
-        uint256 winnersCount,
+        uint32 winnersCount,
         uint256 timestamp
     );
-    event Lottery__PeriodChanged(uint256 period, uint256 timestamp);
-    event Lottery__DelayChanged(uint256 delay, uint256 timestamp);
-    event Lottery__NftRewardCountChanged(uint256 nftCount, uint256 timestamp);
+    event Lottery__DurationChanged(uint64 duration, uint256 timestamp);
+    event Lottery__DelayChanged(uint32 delay, uint256 timestamp);
+    event Lottery__NftRewardCountChanged(uint32 nftCount, uint256 timestamp);
     event Lottery__Paused(uint256 timestamp);
 
     //--------------------------------------------------------------------
@@ -89,7 +87,7 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     error Lottery__TransferFailed();
     error Lottery__ApprovalFailed();
     error NFTLottery__UpkeepNotNeeded(
-        LOTTERY_STATE state,
+        LOTTERY_STATUS state,
         uint256 participantsCount
     );
 
@@ -97,9 +95,11 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     // MODIFIER
 
     // can only change lottery parameters when :
-    // 1-> Lottery is paused AND 2-> Lottery is closed
+    // 1- Lottery is paused AND 2- Lottery is closed
     modifier canChange() {
-        if (paused == 2 || _lotteryState != LOTTERY_STATE.CLOSED) {
+        if (
+            !lotteryState.paused || lotteryState.status != LOTTERY_STATUS.CLOSED
+        ) {
             revert Lottery__ChangesNotAllowed();
         }
         _;
@@ -117,19 +117,28 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
         address _aDaiAddress,
         address _ledingPoolAddress,
         address _nftAddress,
-        uint256 _ticketPrice
-    ) VRFConsumerBaseV2(_vrfCoordinatorV2) {
-        vrfCoordinator = VRFCoordinatorV2Interface(_vrfCoordinatorV2);
-        gasLane = _gasLane;
-        subscriptionId = _subscriptionId;
-        callbackGasLimit = _callbackGasLimit;
-
+        LotteryParams memory _params
+    )
+        ChainlinkUtils(
+            _vrfCoordinatorV2,
+            _subscriptionId,
+            _gasLane,
+            _callbackGasLimit
+        )
+    {
         nft = INFTCollection(_nftAddress);
         dai = IERC20(_daiAddress);
         aDai = IERC20(_aDaiAddress);
         pool = IPool(_ledingPoolAddress);
 
-        ticketBasePrice = _ticketPrice;
+        lotteryState.nftRewardCount = _params.nftRewardCount;
+        lotteryState.winnersPerRound = _params.winnersPerRound;
+        lotteryState.startTimestamp = _params.startTimestamp;
+        lotteryState.duration = _params.duration;
+        lotteryState.delay = _params.delay;
+        lotteryState.ticketPrice = _params.ticketPrice;
+        lotteryState.status = LOTTERY_STATUS.CLOSED;
+        lotteryState.paused = true;
     }
 
     //--------------------------------------------------------------------
@@ -140,8 +149,7 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
             revert Lottery__AlreadyInParticipants();
         }
         uint256 userNftCount = nft.balanceOf(msg.sender);
-        uint256 _entryPrice = ticketBasePrice * (1 + userNftCount);
-
+        uint256 _entryPrice = lotteryState.ticketPrice * (1 + userNftCount);
         uint256 _newId = participants.length;
 
         bool success = dai.transferFrom(msg.sender, address(this), _entryPrice);
@@ -150,11 +158,10 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
         }
 
         _totalDAIDeposited = _totalDAIDeposited + _entryPrice;
-
         _depositToAAVE(_entryPrice);
 
         participants.push(msg.sender);
-        isParticipant[msg.sender] = Participant(_newId, true);
+        isParticipant[msg.sender] = Participant(uint224(_newId), true);
 
         emit Lottery__Enter(msg.sender, _entryPrice, block.timestamp);
     }
@@ -165,7 +172,9 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
         emit Lottery__Quit(msg.sender, amount, block.timestamp);
     }
 
-    function checkUpkeep(bytes calldata checkData)
+    function checkUpkeep(
+        bytes calldata checkData
+    )
         public
         view
         override
@@ -173,20 +182,19 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     {
         // check if lottery need to be opened
         if (keccak256(checkData) == keccak256(hex"01")) {
-            bool isClosed = _lotteryState == LOTTERY_STATE.CLOSED;
-            bool isNotPaused = paused == 2;
-            upkeepNeeded = (isClosed && isNotPaused);
+            bool isClosed = lotteryState.status == LOTTERY_STATUS.CLOSED;
+            upkeepNeeded = (isClosed && !lotteryState.paused);
             performData = checkData;
         }
 
         // check if lottery has reached closing period
         if (keccak256(checkData) == keccak256(hex"02")) {
-            bool isOpen = _lotteryState == LOTTERY_STATE.OPEN;
-            bool lotteryPeriodEnded = block.timestamp >
-                (_lotteryStartTimestamp + lotteryPeriod);
+            bool isOpen = lotteryState.status == LOTTERY_STATUS.OPEN;
+            bool lotteryDurationEnded = block.timestamp >
+                (lotteryState.startTimestamp + lotteryState.duration);
             bool hasParticipants = participants.length > 0;
 
-            upkeepNeeded = (isOpen && lotteryPeriodEnded && hasParticipants);
+            upkeepNeeded = (isOpen && lotteryDurationEnded && hasParticipants);
             performData = checkData;
         }
     }
@@ -195,7 +203,7 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
         (bool upkeepNeeded, ) = checkUpkeep(performData);
         if (!upkeepNeeded) {
             revert NFTLottery__UpkeepNotNeeded(
-                _lotteryState,
+                lotteryState.status,
                 participants.length
             );
         }
@@ -210,7 +218,7 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     }
 
     function fulfillRandomWords(
-        uint256, /* requestId */
+        uint256 /* requestId */,
         uint256[] memory randomWords
     ) internal override {
         uint256 len = randomWords.length;
@@ -227,40 +235,42 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
             }
         }
 
+        uint256 reward = uint256(lotteryState.nftRewardCount);
+
         // quit & mint nft for winners
         for (uint256 i; i < len; ) {
             address winner = winnersAddresses[i];
             // Remove the winner from the partcipant list and give him back his entry cost in DAI
             _quit(winner);
             // Mint the nfts for the winner
-            nft.mint(winner, lotteryNftRewardCount);
+            nft.mint(winner, reward);
             unchecked {
                 ++i;
             }
         }
-        emit Lottery__WinnersPicked(
-            winnersAddresses,
-            lotteryNftRewardCount,
-            block.timestamp
-        );
 
-        _lotteryState = LOTTERY_STATE.CLOSED;
+        lotteryState.status = LOTTERY_STATUS.CLOSED;
+        emit Lottery__WinnersPicked(winnersAddresses, reward, block.timestamp);
     }
 
     //--------------------------------------------------------------------
     // Internal Functions
 
     function open() internal {
-        if (_lotteryState == LOTTERY_STATE.OPEN) revert Lottery__IsNotClosed();
-        _lotteryState = LOTTERY_STATE.OPEN;
-        _lotteryStartTimestamp = block.timestamp + lotteryDelay;
+        if (lotteryState.status == LOTTERY_STATUS.OPEN)
+            revert Lottery__IsNotClosed();
+        lotteryState.status = LOTTERY_STATUS.OPEN;
+        lotteryState.startTimestamp = uint64(
+            block.timestamp + lotteryState.delay
+        );
 
-        emit Lottery__IsOpen(_lotteryStartTimestamp, block.timestamp);
+        emit Lottery__IsOpen(lotteryState.startTimestamp, block.timestamp);
     }
 
     function close() internal {
-        if (_lotteryState != LOTTERY_STATE.OPEN) revert Lottery__IsNotOpen();
-        _lotteryState = LOTTERY_STATE.CALCULATING_WINNER;
+        if (lotteryState.status != LOTTERY_STATUS.OPEN)
+            revert Lottery__IsNotOpen();
+        lotteryState.status = LOTTERY_STATUS.CALCULATING_WINNER;
 
         emit Lottery__IsClosed(block.timestamp);
 
@@ -269,23 +279,20 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
             subscriptionId,
             REQUEST_CONFIRMATIONS,
             callbackGasLimit,
-            uint32(winnersPerLottery)
+            lotteryState.winnersPerRound
         );
-
         emit Lottery__RequestWinners(requestId);
     }
 
     function _quit(address account) internal returns (uint256) {
         uint256 userNftCount = nft.balanceOf(account);
-        uint256 _amount = ticketBasePrice * (1 + userNftCount);
-
+        uint256 _amount = lotteryState.ticketPrice * (1 + userNftCount);
         uint256 _participantId = isParticipant[account].id;
 
         _remove(_participantId);
         delete isParticipant[account];
 
         _withdrawFromAAVE(account, _amount);
-
         _totalDAIDeposited = _totalDAIDeposited - _amount;
 
         return _amount;
@@ -316,8 +323,7 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
             // swap last index with the removed index
             address lastParticipant = participants[lastIndex];
             participants[index] = lastParticipant;
-            isParticipant[lastParticipant].id = index;
-
+            isParticipant[lastParticipant].id = uint224(index);
             participants.pop();
         }
     }
@@ -329,12 +335,12 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
         return participants;
     }
 
-    function getLotteryState() external view returns (LOTTERY_STATE) {
-        return _lotteryState;
+    function getLotteryState() external view returns (State memory) {
+        return lotteryState;
     }
 
     function lotteryStartingTimestamp() external view returns (uint256) {
-        return _lotteryStartTimestamp;
+        return lotteryState.startTimestamp;
     }
 
     function lotteryDAIBalance() external view returns (uint256) {
@@ -344,58 +350,45 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     //--------------------------------------------------------------------
     // Admin Functions
 
-    function _setLotteryTicketBasePrice(uint256 _newTicketPrice)
-        external
-        onlyOwner
-        canChange
-    {
-        ticketBasePrice = _newTicketPrice;
-
+    function _setLotteryTicketBasePrice(
+        uint256 _newTicketPrice
+    ) external onlyOwner canChange {
+        lotteryState.ticketPrice = _newTicketPrice;
         emit Lottery__TicketPriceChanged(_newTicketPrice, block.timestamp);
     }
 
-    function _setWinnersPerLottery(uint256 _winners)
-        external
-        onlyOwner
-        canChange
-    {
-        winnersPerLottery = _winners;
-
+    function _setWinnersPerLottery(
+        uint32 _winners
+    ) external onlyOwner canChange {
+        lotteryState.winnersPerRound = _winners;
         emit Lottery__WinnersPerLotteryChanged(_winners, block.timestamp);
     }
 
-    function _setLotteryPeriod(uint256 _newPeriod)
-        external
-        onlyOwner
-        canChange
-    {
-        lotteryPeriod = _newPeriod;
-
-        emit Lottery__PeriodChanged(_newPeriod, block.timestamp);
+    function _setLotteryDuration(
+        uint64 _newDuration
+    ) external onlyOwner canChange {
+        lotteryState.duration = _newDuration;
+        emit Lottery__DurationChanged(_newDuration, block.timestamp);
     }
 
-    function _setLotteryDelay(uint256 _newDelay) external onlyOwner canChange {
-        lotteryDelay = _newDelay;
-
+    function _setLotteryDelay(uint32 _newDelay) external onlyOwner canChange {
+        lotteryState.delay = _newDelay;
         emit Lottery__DelayChanged(_newDelay, block.timestamp);
     }
 
-    function _setlotteryNftRewardCount(uint256 _newLotteryNftRewardCount)
-        external
-        onlyOwner
-        canChange
-    {
-        lotteryNftRewardCount = _newLotteryNftRewardCount;
-
+    function _setlotteryNftRewardCount(
+        uint32 _newLotteryNftRewardCount
+    ) external onlyOwner canChange {
+        lotteryState.nftRewardCount = _newLotteryNftRewardCount;
         emit Lottery__NftRewardCountChanged(
             _newLotteryNftRewardCount,
             block.timestamp
         );
     }
 
-    function pause(uint256 _state) external payable onlyOwner {
-        if (_state == 1 || _state == 2) {
-            paused = _state;
+    function pause(bool _state) external payable onlyOwner {
+        if (_state != lotteryState.paused) {
+            lotteryState.paused = _state;
             emit Lottery__Paused(block.timestamp);
         }
     }
@@ -403,13 +396,12 @@ contract NFTLottery is VRFConsumerBaseV2, KeeperCompatibleInterface, Ownable {
     function withdrawAccumulatedInterest() external payable onlyOwner {
         uint256 lotteryDaiBalance = _totalDAIDeposited;
         uint256 _aDaiBalance = aDai.balanceOf(address(this));
-
         if (_aDaiBalance > lotteryDaiBalance) {
             uint256 _totalInterest;
             unchecked {
                 _totalInterest = _aDaiBalance - lotteryDaiBalance;
             }
-            _withdrawFromAAVE(owner(), _totalInterest);
+            _withdrawFromAAVE(msg.sender, _totalInterest);
         }
     }
 }
